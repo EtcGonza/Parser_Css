2b48d5696f91a3374a56f7869bb65915d830950d
EtcGonza <xetchegaraygonzalo@gmail.com>
2020-11-07 16:45:26 -0300

    Escribe propiedades y selectores en un archivo

--- pruebaLucio.hs
+++ pruebaLucio.hs
@@ -1,5 +1,5 @@
 
-data ListCss a = Vacio |Elemento a (ListCss a ) |Propiedad a (ListCss a) deriving (Show)
+data ListCss a = Vacio | Elemento a (ListCss a ) | Propiedad a (ListCss a) deriving (Show)
 
 --elemento h1 -> elemento -> elemento -> [font-size: 10px] -> Vacio
 -- El tipo se define como ".fondo-azul" Vacio
@@ -20,10 +20,32 @@ crearListaCss (Elemento a Vacio) (Propiedad x propiedades) = Elemento a (Propied
 crearListaCss (Elemento a selectores) (Propiedad x propiedades) = Elemento a (crearListaCss selectores (Propiedad x propiedades) )
 
 --Funcion que convierte una lista CSS en codigo CSS
-generarCss :: ListCss String -> IO ()
-generarCss Vacio = putStrLn "El input es vacio, no se puede escribir."
-generarCss listaCSS = appendFile "style.css" (css2String (a) ++ "{" ++ propiedades ++ "}" ++ "\n")
+generarCss :: ListCss String -> ListCss String -> IO ()
+generarCss Vacio Vacio = putStrLn "El input es vacio, no se puede escribir."
+generarCss listaSelectores listaPropiedades = appendFile "style.css" (selectores2String (listaSelectores) ++ " {" ++ "\n" ++ (propiedades2String listaPropiedades) ++ "\n" ++ "}")
 
+--Funciones de apoyo
+selectores2String :: ListCss String -> String
+selectores2String Vacio = []
+selectores2String (Elemento a Vacio) = a
+selectores2String (Elemento a l) = a ++ " " ++ (selectores2String l)
+
+propiedades2String :: ListCss String -> String
+propiedades2String Vacio = []
+propiedades2String (Propiedad a Vacio) = " " ++ a ++ ";"
+propiedades2String (Propiedad a l) = " " ++ a ++ ";" ++ "\n" ++ (propiedades2String l)
+
+-- generarLista :: ListCss String -> String
+-- generarLista (Propiedad b Vacio) = b
+-- generarLista (Propiedad b l) = " "  ++  b ++ (generarLista l)
+-- generarLista (Elemento a l) = a ++ (generarLista l)
+
+-- identificarTipo :: ListCss String -> String
+-- identificarTipo Vacio = error "debe ingresar un valor"
+-- identificarTipo (Elemento a Vacio) 
+--     | head( css2String (Elemento a Vacio) ) == '.' = "Clase"
+--     | head( css2String (Elemento a Vacio) ) == '#' = "Id"
+--     | otherwise = "Etiqueta"
 
 --pruebas
 s1 = generarSelectores "h1" Vacio
@@ -31,8 +53,8 @@ s2 = generarSelectores "#id" s1
 s3 = generarSelectores ".test" s2
 
 p1 = generarPropiedades "color: red" Vacio
-p2 = generarPropiedades "padding" p1
-p3 = generarPropiedades "font-size" p2
+p2 = generarPropiedades "padding: 20px" p1
+p3 = generarPropiedades "font-size: 12px" p2
 
 listaCss1 = crearListaCss s1 p1
 listaCss2 = crearListaCss s1 p2
@@ -43,16 +65,3 @@ listaCss6 = crearListaCss s2 p3
 listaCss7 = crearListaCss s3 p1
 listaCss8 = crearListaCss s3 p2
 listaCss9 = crearListaCss s3 p3
-
-
---Funciones de apoyo
-css2String :: ListCss String -> String
-css2String Vacio = []
-css2String (Elemento a _) = a
-
-identificarTipo :: ListCss String -> String
-identificarTipo Vacio = error "debe ingresar un valor"
-identificarTipo (Elemento a Vacio) 
-    | head( css2String (Elemento a Vacio) ) == '.' = "Clase"
-    | head( css2String (Elemento a Vacio) ) == '#' = "Id"
-    | otherwise = "Etiqueta"
